2aca02391c6d49a6bd0fc14367408691ccbbd76f
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-14 16:01:55 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let textOne = "Uwielbiam JavaScript";
+let textTwo = "Jestem świetnym programistą";
+
+if (textOne.length < textTwo.length) {
+    console.log(textTwo);
+} else if (textOne.length > textTwo.length) {
+    console.log(textOne);
+}
+
